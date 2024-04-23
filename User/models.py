574e43fc4e8cb52dc9c6860a1cfdfbf384c15688
Rohit10jr <rohitjworkspace@gmail.com>
2024-04-23 22:24:59 +0530

    erp user model serializers

---
+++ User/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from simple_history.models import HistoricalRecords
+
+
+# Create your models here.
+class User(AbstractUser):
+    name = models.TextField(max_length=200, null=False)
+    email = models.EmailField(unique=True, null=False)
+    created_by = models.TextField(default="primary",max_length=200, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    altered_by = models.TextField(max_length=200, null=True, blank=True)
+    history = HistoricalRecords()
+    
+    username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+
+
+class user_group(models.Model):
+    user_group_name = models.TextField(max_length=200, null=False, unique=True) #should be unique??
+    backdated_days = models.IntegerField(default=60,null=True)
+    created_by = models.TextField(max_length=200, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    altered_by = models.TextField(max_length=200, null=True, blank=True)
+    history = HistoricalRecords()
+    def __str__(self):
+        return self.user_group_name
+
+
+
+
+class transaction_right(models.Model):
+    transactions = models.TextField(max_length=200, null=False)
+    created_by = models.TextField(max_length=200, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.transactions
+
+
+class user_right(models.Model):
+    user_group_id = models.ForeignKey(to=user_group, null=False, on_delete=models.PROTECT)
+    transaction_id = models.ForeignKey(to=transaction_right, null=False, on_delete=models.PROTECT)
+    can_create = models.BooleanField(default=False)
+    can_alter = models.BooleanField(default=False)
+    can_delete = models.BooleanField(default=False)
+    can_view = models.BooleanField(default=False)
+    created_by = models.TextField(max_length=200, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    altered_by = models.TextField(max_length=200, null=True, blank=True)
+    history = HistoricalRecords()
+    class Meta:
+        unique_together = ('user_group_id', 'transaction_id',)
---
+++ User/serializers.py
@@ -0,0 +1,113 @@
+from rest_framework import serializers
+from rest_framework.views import APIView
+from .models import User, transaction_right, user_group, user_right
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'name', 'email', 'password','is_superuser','created_by']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'id':{'read_only': True},
+            'created_on':{'read_only': True},
+            'altered_by': {'write_only': True}
+        }
+
+
+    def update(self, instance, validated_data):
+        if(instance.password):
+            for attr, value in validated_data.items():
+                if attr == 'password' and value!=None:
+                    instance.set_password(value)
+                elif attr == 'password' and value==None:
+                    pass
+                else:
+                    setattr(instance, attr, value)
+            instance.save()
+            return instance
+        else:
+            instance.save()
+            return instance
+
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+class UsernameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'name', 'email']
+
+
+class UserGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = user_group
+        fields = '__all__'
+        extra_kwargs = {
+            'id':{'read_only':True},
+            'created_on':{'read_only':True},
+            'altered_by':{'write_only':True}
+        }
+
+        def create(self, validated_data):
+            instance = self.Meta.model(**validated_data)
+            instance.save()
+            return instance
+        
+        def update(self, instance, validated_data):
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save()
+            return instance
+
+
+class UserRightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = user_right
+        fields = '__all__'
+        extra_kwargs = {
+            'id':{'read_only':True},
+            'created_on':{'read_only':True },
+            'altered_on':{'write_only':True }
+        }
+
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
+    
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+
+class TransactionRightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = transaction_right
+        fields = '__all__'
+        extra_kwargs = {
+            'id':{'read_only':True},
+            'creatd_on':{'read_only':True},
+            'altered_by':{'write_only':True}
+        }
+
+
+class GetUserRightSerializer(serializers.ModelSerializer):
+    trancstion_id = TransactionRightSerializer()
+    class Meta:
+        model = user_right
+        # fields = '__all__' 
+        exclude = ('user_group_id')
+        extra_kwargs = {
+            "id":{'read_only':True},
+            "created_on":{'read_only':True},
+            "altered_by":{'write_only':True}
+        }
